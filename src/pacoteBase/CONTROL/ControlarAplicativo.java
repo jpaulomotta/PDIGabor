b3116fda610a83a3438b5e931a31a73a3eb7a4df
Joao Paulo Motta O. Silva <jpaulo.motta@gmail.com>
2016-06-07 19:34:01 -0300

    Corrige bug de morfologia nao estar persistindo apos realizar outras operações

--- src/pacoteBase/CONTROL/ControlarAplicativo.java
+++ src/pacoteBase/CONTROL/ControlarAplicativo.java
@@ -21,10 +21,10 @@ public class ControlarAplicativo implements ActionListener {
 	private char[][]            imagemAtual;
 
 	private boolean             estadoDesenho;
-	
+
 	public static final int OPERACAO_ABRIR = 1, OPERACAO_SALVAR = 2;
 
-	
+
 	//*******************************************************************************************
 	public ControlarAplicativo( )
 	{
@@ -32,11 +32,11 @@ public class ControlarAplicativo implements ActionListener {
 		pnCenario.showPanel();
 		estadoDesenho  = false;
 	}
- 
-	
+
+
 	//*******************************************************************************************
 	// METODO PARA CONTROLE DOS BOTOES DO APLICATIVO
-	
+
 	public void actionPerformed(ActionEvent e)
 	{
 		String comando, nomeArquivo;
@@ -60,174 +60,161 @@ public class ControlarAplicativo implements ActionListener {
 			nomeArquivoImagemDada = pnCenario.escolherArquivo ( OPERACAO_ABRIR );
 			carregarImagem();
 		}
-		
-		
-//BOTAO MORFOLOGIAA *****************************************
-		
+
+
+		//BOTAO MORFOLOGIAA *****************************************
+
 		if ( comando.equals( "botaoOpMorfologica" ) && estadoDesenho ) {
-		
-			
+
+
 			if(pnCenario.isTransictionsSelected()){   
 				//if (pnCenario.getbtOpMorfologica2() ==2){
-				
-				
+
+
 				char[][] temp = controleImagem.copiarImagem(imagemAtual);
-				
+
 				imagemAtual = controleImagem.aplicaMorfologia(imagemAtual);
 				pnCenario.limpaPainelCen();
 				controleImagem.mostrarImagem(temp, desenhoCentro);
 				pnCenario.limpaPainelDir();
 			}
-					
-				if (pnCenario.isNewImageSelected())
-					imagemAtual = controleImagem.aplicaMorfologia(imagemAtual);
-					
-					pnCenario.limpaPainelDir();
-					controleImagem.mostrarImagem(imagemAtual, desenhoDireita);	
-				}	
-			
-	//	}
+
+			if (pnCenario.isNewImageSelected())
+				imagemAtual = controleImagem.aplicaMorfologia(imagemAtual);
+
+			pnCenario.limpaPainelDir();
+			controleImagem.mostrarImagem(imagemAtual, desenhoDireita);	
+		}	
+
+		//	}
 
 
 
-//FILTRO DE GABORRR****************************************************
-		
-		
+		//FILTRO DE GABORRR****************************************************
+
+
 		if ( comando.equals( "botaoAcao1" )&& estadoDesenho  )  {
 
-		  
-		    //Criando buffer image a partir do arquivo dado. 
-		    BufferedImage bufferedImage = controleImagem.transformarMatriz2Buffer ( imagemAtual );
-		    // passando parametros para classe GaborFilter e salvando a imagem filtrada no disco
-		     
-		  
-	//*******************************************************
-	//APLICA FILTRO GABOR DE ACORDO COM PARAMETRO SELECIONADO
-		  
-		if (pnCenario.getbtGabor1() ==2){
-			
-			
-			if (pnCenario.getbtGabor12() ==2) {
-				bufferedImage = (BufferedImage) new GaborFiltro(1, new double[] {0}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-			}	
-			
-			if (pnCenario.getbtGabor22() ==2) {
-				bufferedImage = (BufferedImage) new GaborFiltro(1, new double[] {Math.PI/4}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-			}	
-			
-			if (pnCenario.getbtGabor32() ==2) {
-				bufferedImage = (BufferedImage) new GaborFiltro(1, new double[] {Math.PI/2}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-			}	
-			
-			if (pnCenario.getbtGabor42() ==2) {
-				bufferedImage = (BufferedImage) new GaborFiltro(1, new double[] {Math.PI/0.75}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-			}	
-			
-			controleImagem.mostrarImagem(bufferedImage, desenhoDireita);	
-		}
 
-if (pnCenario.getbtGabor2() ==2){
-			
-			
-			if (pnCenario.getbtGabor12() ==2) {
-				bufferedImage = (BufferedImage) new GaborFiltro(2, new double[] {0}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-			}	
-			
-			if (pnCenario.getbtGabor22() ==2) {
-				bufferedImage = (BufferedImage) new GaborFiltro(2, new double[] {Math.PI/4}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-			}	
-			
-			if (pnCenario.getbtGabor32() ==2) {
-				bufferedImage = (BufferedImage) new GaborFiltro(2, new double[] {Math.PI/2}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-			}	
-			
-			if (pnCenario.getbtGabor42() ==2) {
-				bufferedImage = (BufferedImage) new GaborFiltro(2, new double[] {Math.PI/0.75}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-			}	
+			//Criando buffer image a partir do arquivo dado. 
+			BufferedImage bufferedImage = controleImagem.transformarMatriz2Buffer ( imagemAtual );
+			// passando parametros para classe GaborFilter e salvando a imagem filtrada no disco
+
+
+			//*******************************************************
+			//APLICA FILTRO GABOR DE ACORDO COM PARAMETRO SELECIONADO
+
+			if (pnCenario.getbtGabor1() ==2){
+				if (pnCenario.getbtGabor12() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(1, new double[] {0}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor22() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(1, new double[] {Math.PI/4}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor32() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(1, new double[] {Math.PI/2}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor42() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(1, new double[] {Math.PI/0.75}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+			}
+
+			if (pnCenario.getbtGabor2() ==2){
+				if (pnCenario.getbtGabor12() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(2, new double[] {0}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor22() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(2, new double[] {Math.PI/4}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor32() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(2, new double[] {Math.PI/2}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor42() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(2, new double[] {Math.PI/0.75}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+			}
+
+
+			if (pnCenario.getbtGabor3() ==2){
+
+				if (pnCenario.getbtGabor12() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(3, new double[] {0}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor22() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(3, new double[] {Math.PI/4}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor32() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(3, new double[] {Math.PI/2}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor42() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(3, new double[] {Math.PI/0.75}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+			}
+
+			if (pnCenario.getbtGabor4() ==2){
+
+
+				if (pnCenario.getbtGabor12() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(4, new double[] {0}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor22() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(4, new double[] {Math.PI/4}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor32() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(4, new double[] {Math.PI/2}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+
+				if (pnCenario.getbtGabor42() ==2) {
+					bufferedImage = (BufferedImage) new GaborFiltro(4, new double[] {Math.PI/0.75}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
+				}	
+			}
 			
 			controleImagem.mostrarImagem(bufferedImage, desenhoDireita);
-		}
+			imagemAtual = controleImagem.criarImagemCinza(bufferedImage);
+		} 
 
-		
-if (pnCenario.getbtGabor3() ==2){
-	
-	
-	if (pnCenario.getbtGabor12() ==2) {
-		bufferedImage = (BufferedImage) new GaborFiltro(3, new double[] {0}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-	}	
-	
-	if (pnCenario.getbtGabor22() ==2) {
-		bufferedImage = (BufferedImage) new GaborFiltro(3, new double[] {Math.PI/4}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-	}	
-	
-	if (pnCenario.getbtGabor32() ==2) {
-		bufferedImage = (BufferedImage) new GaborFiltro(3, new double[] {Math.PI/2}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-	}	
-	
-	if (pnCenario.getbtGabor42() ==2) {
-		bufferedImage = (BufferedImage) new GaborFiltro(3, new double[] {Math.PI/0.75}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-	}	
-	
-	controleImagem.mostrarImagem(bufferedImage, desenhoDireita);
-}
-
-
-if (pnCenario.getbtGabor4() ==2){
-	
-	
-	if (pnCenario.getbtGabor12() ==2) {
-		bufferedImage = (BufferedImage) new GaborFiltro(4, new double[] {0}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-	}	
-	
-	if (pnCenario.getbtGabor22() ==2) {
-		bufferedImage = (BufferedImage) new GaborFiltro(4, new double[] {Math.PI/4}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-	}	
-	
-	if (pnCenario.getbtGabor32() ==2) {
-		bufferedImage = (BufferedImage) new GaborFiltro(4, new double[] {Math.PI/2}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-	}	
-	
-	if (pnCenario.getbtGabor42() ==2) {
-		bufferedImage = (BufferedImage) new GaborFiltro(4, new double[] {Math.PI/0.75}, 100, 0, 1, 3, 3).filter(bufferedImage, null);
-	}	
-	
-	controleImagem.mostrarImagem(bufferedImage, desenhoDireita);
-	imagemAtual = controleImagem.criarImagemCinza(bufferedImage);
-}
-
-		
-} 
-		
 		//ZOOM **************************************************
-		
+
 		if (comando.equals("botaoZoom") && estadoDesenho) {
-	        
+
 			int percentageZoom = 0; // porcentagem de zoom escolhida
-			
+
 			if(pnCenario.getBotaoZoomIn25() == 2){
 				percentageZoom = 25;		
 			}
-			
+
 			if(pnCenario.getBotaoZoomIn50() == 2){
 				percentageZoom = 50;		
 			}
-						
+
 			if (pnCenario.getBotaoZoomIn75() == 2) {				
 				percentageZoom = 75;
 			}
-			
+
 			if(pnCenario.getBotaoZoomIn100() == 2){
 				percentageZoom = 100;
 			}
-			
+
 			if (pnCenario.getBotaoZoomOut25() == 2){
 				percentageZoom = -25;
 			}
-			
+
 			if(pnCenario.getBotaoZoomOut50() == 2){
 				percentageZoom = -50;
 			}
-			
+
 			if(pnCenario.getBotaoZoomOut75() == 2){
 				percentageZoom = -75;
 			}
@@ -237,11 +224,11 @@ if (pnCenario.getbtGabor4() ==2){
 			//TEXTFILD
 			if(pnCenario.getbtZoomText()!=0){
 				percentageZoom = pnCenario.getbtZoomText();
-				
+
 			}
-			
+
 			controlZoom(percentageZoom);	
-			
+
 		}
 
 		if ( comando.equals( "botaoSalva" ) && estadoDesenho ) {
@@ -255,11 +242,11 @@ if (pnCenario.getbtGabor4() ==2){
 			pnCenario.resetaSistema();
 		}
 	}
-	
-	
-	
+
+
+
 	private void carregarImagem() {
-		
+
 		if ( nomeArquivoImagemDada != null ) {
 			controleImagem = new ControlarImagem( nomeArquivoImagemDada, desenhoCentro );
 			estadoDesenho  = true;
@@ -274,24 +261,24 @@ if (pnCenario.getbtGabor4() ==2){
 
 
 	private void controlZoom(int percentageZoom) {		
-							
+
 		if(pnCenario.isTransictionsSelected()){   
-			
+
 			char[][] temp = controleImagem.copiarImagem(imagemAtual);
-			
+
 			imagemAtual = controleImagem.aplicaZoom(imagemAtual, percentageZoom);
 			pnCenario.limpaPainelCen();
 			controleImagem.mostrarImagem(temp, desenhoCentro);
 			pnCenario.limpaPainelDir();
 		}
-				
+
 		if (pnCenario.isNewImageSelected()){
 			imagemAtual = controleImagem.aplicaZoom(imagemAtual, percentageZoom);
-			
+
 			pnCenario.limpaPainelDir();
 			controleImagem.mostrarImagem(imagemAtual, desenhoDireita);
 		}
-			
+
 	}
-	
+
 }
